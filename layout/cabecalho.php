695701dd5a468e813461355293094c2dfad9f7fa
VinTesta <70596205+VinTesta@users.noreply.github.com>
2022-02-16 14:33:56 -0300

    Desc tela admin

--- layout/cabecalho.php
+++ layout/cabecalho.php
@@ -8,7 +8,7 @@ require_once("../layout/toast-alert.php");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Clientes Kabum</title>
+    <title>VemPraKaBuM - Sistema de Gestão</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
--- view/admin-home.php
+++ view/admin-home.php
@@ -2,6 +2,21 @@
 require_once("../layout/cabecalho.php");
 ?>
 
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="shadow p-3 mb-4 mt-4 bg-body rounded text-center h3">Página principal</div>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-md-10 text-center">
+            <p>A utização e viagem entre paginas ocorre inteiramente pelo navbar do sistema</p>
+            <p>Apenas o sistema de clientes está em funcionamento e pode ser acessado no link de clientes da navbar ou por esse <a href="pesquisa-cliente">aqui</a></p>
+        </div>
+    </div>
+
+</div>
+
 <?php
 require_once("../layout/rodape.php");
 ?>
